a7ea3362e7d6ea6120e84f4b956e0d5a7673ac2a
lkdjiin <xavier.nayrac@gmail.com>
2013-05-29 16:23:01 +0200

    Add shadow name in the shadow select box

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,22 @@
 module PagesHelper
-  def shadow_tag shadow_name
-    image_tag "shadows/shadow_#{shadow_name}.png",
-      class: "shadow-thumb",
-      onclick: "window.maker.shape_changed('#{shadow_name}')",
-      size: "140x140"
+  def shadow_types
+    [
+      [ "basic", "Basic" ],
+      [ "curled", "Curled"],
+      [ "curved_hz", "Curved horizontal" ],
+      [ "curved_hz_bottom", "Curved horizontal (bottom only)" ],
+      [ "curved_hz_top", "Curved horizontal (top only)" ],
+      [ "perspective", "Left perspective" ],
+      [ "perspective_r", "Right perspective" ],
+      [ "curved_vt", "Curved vertical" ],
+      [ "see_through_tape", "See through tape" ],
+      [ "wrinkled", "Wrinkled" ]
+    ]
+  end
+
+  def shadow_tag type
+    image_tag("shadows/shadow_#{type}.png",
+      onclick: "window.maker.shape_changed('#{type}')",
+      size: "140x140")
   end
 end
